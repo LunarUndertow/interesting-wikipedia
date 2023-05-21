4b52ead48d3ba2fbff07446b4f13a450f0925dea
LunarUndertow <noreply>
2023-05-21 23:13:24 +0300

    pageCard component fetches and renders page title, thumbnail, and extract based on the path given as a prop

--- src/App.js
+++ src/App.js
@@ -32,22 +32,10 @@ function App() {
     return (
         <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-            </a>
-            <PageCard page={randomPageName()} />
-            <PageCard page={randomPageName()} />
-            <PageCard page={randomPageName()} />
-            <PageCard page={randomPageName()} />
+            <PageCard path={randomPageName()} />
+            <PageCard path={randomPageName()} />
+            <PageCard path={randomPageName()} />
+            <PageCard path={randomPageName()} />
         </header>
         </div>
     );
--- src/Components/pageCard.js
+++ src/Components/pageCard.js
@@ -1,8 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function PageCard(props) {
+    const path = props.path;
+    const [page, setPage] = useState(null)
+
+    useEffect(() => {
+        if (path) {
+            axios.get("https://en.wikipedia.org/api/rest_v1/page/summary/" + path.substring(2))
+                .then((response) => {
+                    setPage(response.data);
+                });
+        }
+    }, [path]);
+    
+
+    const title = () => {
+        if (page) return page.title;
+    };
+
+
+    const extract = () => {
+        if (page) return page.extract;
+    };
+
+
+    const thumbnail = () => {
+        if (page && page.thumbnail) return <img src={page.thumbnail.source} alt="Wikipedia image thumbnail" />;
+    }
+
+
     return (
-        <div>
-            { props.page }
-        </div>
+        <span>
+            <div>
+                { title() }
+            </div>
+            <div>
+                { thumbnail() }
+            </div>
+            <div>
+                { extract() }
+            </div>
+        </span>
     );
 }
 
