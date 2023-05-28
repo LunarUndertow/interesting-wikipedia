b3afad6b6b3cf44c4c746d82ffbb9ea3e2f566ea
LunarUndertow <noreply>
2023-05-28 03:05:19 +0300

    Added a refresh button to trigger a re-render and get new articles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { load } from 'cheerio';
+import Button from '@mui/material/Button';
 import PageCard from './Components/pageCard';
 
 function App() {
     const [pageNames, setPageNames] = useState(null);
+    const [dummyState, setDummyState] = useState(1);
 
     useEffect(() => {
         axios.get("https://en.wikipedia.org/api/rest_v1/page/html/Wikipedia:Unusual_articles",
@@ -29,14 +31,23 @@ function App() {
     }
 
 
+    const refresh = () => {
+        setDummyState((dummyState + 1) % 2);
+    }
+
+
     return (
         <div className="App">
         <header className="App-header">
+            <div>
                 <PageCard path={randomPageName()} />
                 <PageCard path={randomPageName()} />
                 <PageCard path={randomPageName()} />
                 <PageCard path={randomPageName()} />
+            </div>
+            <Button variant="outlined" size="large" onClick={refresh}>Refresh</Button>
         </header>
+        
         </div>
     );
 }
