fffe56763136626fd39edaff81b547d925349994
LunarUndertow <noreply>
2023-06-03 02:42:44 +0300

    Replace relative links with absolute ones in the fetched article so they lead where they should

--- src/Components/modalPage.js
+++ src/Components/modalPage.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
+import { load } from 'cheerio';
 
 function ModalPage(props) {
     const pageUrl = props.pageUrl;
@@ -18,10 +19,25 @@ function ModalPage(props) {
     }, [pageUrl]);
 
 
-    // if a page exists, render a sanitized version of it
+    // Sanitize the fetched page and replace relative links with absolute ones
+    const processedHtml = () => {
+        const sanitizedPage = DOMPurify.sanitize(page);
+        const html = load(sanitizedPage)
+        html('a').each((i, elem) => {
+            if (html(elem).attr('href') && html(elem).attr('href').startsWith('./')) {
+                const path = html(elem).attr('href').substring(1)
+                html(elem).attr('href', 'https://en.wikipedia.org/wiki' + path);
+            }
+        });
+        const pageWithAbsoluteLinks = html.html();
+        return parse(pageWithAbsoluteLinks);
+    };
+
+
+    // if a page has been set, process and render it
     return (
         <div>
-            {page ? parse(DOMPurify.sanitize(page)) : ''}
+            {page ? processedHtml() : ''}
         </div>
     );
 }
